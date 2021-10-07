d2a39b2b75bed9388d1e11b300568fb164489a20
Thijs Drykoningen <thijsdrykoningen@hotmail.nl>
2021-10-07 11:17:36 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,22 @@
       <div class="d-flex align-items-center flex-shrink-0 text-decoration-none">
         <div class="h2 col-6 titel1">Chat</div>
         <!-- input username -->
-        <input class="inputfield1 fw-semibold col-6" placeholder="Gebruikersnaam" v-model="username"/>
+        <input class="inputfield1 fw-semibold col-6" placeholder="Gebruikersnaam" v-model="username" />
       </div>
       <!-- input zoeken -->
       <div class="d-flex align-items-center flex-shrink-0 text-decoration-none inputfield2 col-12">
-        <i class="fa fa-search searchicon"></i><div class="textzoeken">Zoeken in gesprek</div>
+        <i class="fa fa-search searchicon"></i>
+        <div class="textzoeken">Zoeken in gesprek</div>
       </div>
-       <!-- Scrollarea + messages -->
+      <!-- Scrollarea + messages -->
       <div class="list-group list-group-flush border-bottom border-top scrollarea">
-        <div class="list-group-item list-group-item-action py-3 lh-tight msg1" v-for="message in messages" :key="message">
-           <!-- Username message -->
+        <div class="list-group-item list-group-item-action py-3 lh-tight msg1" v-for="message in messages"
+          :key="message">
+          <!-- Username message -->
           <div class="d-flex w-100 align-items-center justify-content-between">
             <strong class="mb-1">{{ message.username }}</strong>
           </div>
-           <!-- Content message -->
+          <!-- Content message -->
           <div class="col-10 mb-1 small">{{ message.message }}</div>
         </div>
       </div>
@@ -31,7 +33,7 @@
   </div>
 </template>
 <script>
-//import the vue instance and Pusher Database
+  //import the vue instance and Pusher Database
   import {
     ref,
     onMounted
@@ -48,7 +50,7 @@
 
       onMounted(() => {
         Pusher.logToConsole = true;
-        
+
         //unique key for Pusher
         const pusher = new Pusher('3d4cf2aefca809e0f444', {
           cluster: 'eu'
@@ -61,7 +63,7 @@
         });
       })
 
-      //Post method message to port:8000
+      //Post method message to port:8000 haalt data op
       const submit = async () => {
         await fetch('http://localhost:8000/api/messages', {
           method: 'POST',
@@ -86,7 +88,6 @@
       }
     }
   }
-
 </script>
 
 <style>
@@ -103,15 +104,16 @@
   .form {
     color: #003580;
   }
-  .inputfield1{
+
+  .inputfield1 {
     font-size: 1rem;
     padding: 5px 5px 5px 12px;
-    color: #646464;
+    color: #003580;
     border-width: 1px;
-    border-style:solid;
-    border-color:#E6EBF3;
+    border-style: solid;
+    border-color: #E6EBF3;
     border: 1px solid #E6EBF3 !important;
-      border-radius: 15px;
+    border-radius: 15px;
   }
 
   .list-group-item {
@@ -143,40 +145,46 @@
     background-color: #003580;
   }
 
-  .msg1{
+  .msg1 {
     background-color: #E6EBF3;
     border-radius: 10px;
     margin-top: 5px;
   }
-  .titel1{
-    color:#003580;
+
+  .titel1 {
+    color: #003580;
     font-weight: bold;
   }
-  .inputfield2{
-      margin-top: 10px;
-      padding: 5px 5px 5px 8px;
-      color:#003580;
-      background-color: #E6EBF3;
-      border: none;
-      border-radius: 15px;
-      height: 35px;
+
+  .inputfield2 {
+    margin-top: 10px;
+    padding: 5px 5px 5px 8px;
+    color: #003580;
+    background-color: #E6EBF3;
+    border: none;
+    border-radius: 15px;
+    height: 35px;
   }
-  .textzoeken{
-    margin-left: 10px;
-  }
-  .searchicon{
+
+  .textzoeken {
     margin-left: 10px;
   }
 
-.list-group-flush>.list-group-item {
+  .searchicon {
+    margin-left: 10px;
+  }
+
+  .list-group-flush>.list-group-item {
     border-width: 0 0 1px;
     border-radius: 15px;
-}
-.border-bottom {
-    border-bottom: 1px solid #E6EBF3!important;
-}
-.form-control{
-      border: 1px solid #E6EBF3 !important;
-      border-radius: 15px;
-}
+  }
+
+  .border-bottom {
+    border-bottom: 1px solid #E6EBF3 !important;
+  }
+
+  .form-control {
+    border: 1px solid #E6EBF3 !important;
+    border-radius: 15px;
+  }
 </style>
